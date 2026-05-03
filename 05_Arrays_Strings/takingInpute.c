dcd4a315ceb1c26b342b0d1d45d3f7df2ff793be
Roshan9623 <dhangarroshan321@gmail.com>
2026-05-03 23:40:10 +0530

    added takingInpute.c

---
+++ 05_Arrays_Strings/takingInpute.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+    int main(){
+
+        int arr[3];
+        printf("Enter first element : ");
+        scanf("%d", & arr[0]);
+
+        printf("Enter sec element : ");
+        scanf("%d", & arr[1]);
+
+        printf("Enter third element : ");
+        scanf("%d", & arr[2]);
+
+        printf("%d", arr[2]);
+
+
+        return 0;
+    }
