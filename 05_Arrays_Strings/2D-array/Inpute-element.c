fbb7ecddd25e41546c5c00c41a733cfbc921735b
Roshan9623 <dhangarroshan321@gmail.com>
2026-05-17 11:55:33 +0530

    added Inpute-element.c

---
+++ 05_Arrays_Strings/2D-array/Inpute-element.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+    int main(){
+
+        int r;
+        printf("Enter a number row: ");
+        scanf("%d",& r);
+
+        int c;
+        printf("Enter a number column: ");
+        scanf("%d",& c);
+
+        int arr[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                scanf("%d",& arr[i][j]);
+            }
+        }
+        printf("\n");
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                printf("%d ",arr[i][j]);
+            }
+            printf("\n");
+        }
+        return 0;
+    }
