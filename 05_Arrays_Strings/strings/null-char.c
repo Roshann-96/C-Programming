39e23cd85771cdcb8ba5ff4022b63a2b6eccb31b
Roshan9623 <dhangarroshan321@gmail.com>
2026-05-22 11:35:20 +0530

    added null-char

---
+++ 05_Arrays_Strings/strings/null-char.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//#include<string.h>
+    int main(){
+        char arr[]="hello world";
+        int i=0;
+        
+        while(arr[i]!= '\0'){
+            printf("%c",arr[i]);
+            i++;
+        }
+        return 0;
+    }
