284f2a89e94fbf68a3fa0ce3359830fc23b179ec
Roshan9623 <dhangarroshan321@gmail.com>
2026-06-01 00:53:39 +0530

    added input-change.c

---
+++ 06_Pointers/input-change.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+    int main(){
+
+        char *ptr ="collage wallah";
+        printf("%s\n",ptr);
+        ptr="physics wallah";
+        printf("%s",ptr);
+
+        return 0;
+    }
