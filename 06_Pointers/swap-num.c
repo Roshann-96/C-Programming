0657811dd6cbfa60179b67e740ec1060cf6b16be
Roshan9623 <dhangarroshan321@gmail.com>
2026-06-01 23:36:08 +0530

    added swap-num.c

---
+++ 06_Pointers/swap-num.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//#include <String.h>
+void swap(int *a,int *b){
+
+    int temp = *a;
+     *a = *b;
+     *b = temp;
+     
+}
+
+int main(){
+
+    int x =10;
+    int y =20;
+    printf("before swap: x = %d  y = %d \n",x ,y);
+
+    swap(&x ,&y);
+    printf("after swap : x = %d  y = %d",x , y);
+
+    return 0;
+}
