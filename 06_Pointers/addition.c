81e5d92ebf372cbc42c6d9b7507399d7717efe58
Roshan9623 <dhangarroshan321@gmail.com>
2026-06-02 23:34:03 +0530

    added addition.c

---
+++ 06_Pointers/addition.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void add(int *a,int *b){
+
+     int sum= *a + *b;
+     printf("%d or %d sum is :   %d ",*a ,*b ,sum);
+   
+}
+
+int main(){
+
+    int x;
+    printf("Enter 1st num:  ");
+    scanf("%d", &x);
+
+    int y;
+    printf("Enter 2nd num: ");
+    scanf("%d", &y);
+
+    add( &x , &y);
+
+     return 0;
+}
