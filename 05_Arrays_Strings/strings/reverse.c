b2c1c971e224f3c5e41baa32618d69713245c315
Roshan9623 <dhangarroshan321@gmail.com>
2026-06-09 22:53:25 +0530

    added reverse.c

---
+++ 05_Arrays_Strings/strings/reverse.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+    int main(){
+
+        char str[40];
+        puts("entre a string");
+        scanf("%s", str);
+
+        int size = 0;
+        int k = 0;
+        
+        //size check
+        while(str[k]!= '\0'){
+            size++; //7
+            k++; //7
+
+        }
+        // reverse
+        for( int i=0,j=size-1;i<=j;i++,j--){
+            int temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+
+        }
+        puts("The reverse string is ");
+        puts(str);
+
+        return 0;
+    }
